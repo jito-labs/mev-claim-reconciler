08503717fd7f151ab5c5bc8b6aa2f784f3e77302
segfaultdoctor <17258903+segfaultdoc@users.noreply.github.com>
2024-05-02 16:39:50 -0400

    wip

--- bin/send_distributions.rs
+++ bin/send_distributions.rs
@@ -6,6 +6,7 @@
 use clap::Parser;
 use crossbeam::channel::{unbounded, Sender};
 use futures::future::join_all;
+use log::*;
 use mev_claim_reconciler::{append_to_csv_file, read_csv_from_file, FlattenedDistribution};
 use solana_program::hash::Hash;
 use solana_program::system_instruction::transfer;
@@ -40,7 +41,9 @@ struct Args {
 }
 
 fn main() {
-    println!("Starting...");
+    env_logger::init();
+
+    info!("Starting...");
 
     let args: Args = Args::parse();
     let runtime = Arc::new(Runtime::new().unwrap());
@@ -56,11 +59,11 @@ fn main() {
     let rt = runtime.clone();
     let c_exit = exit.clone();
     ctrlc::set_handler(move || {
-        println!("received exit signal");
+        info!("received exit signal");
         c_exit.store(true, Ordering::Relaxed);
         while let Ok(dist) = rx.recv() {
             if let Err(e) = append_to_csv_file(&dist, &completed_distributions_path) {
-                println!(
+                error!(
                     "error writing distribution to file: [receiver={}, amount_lamports={}, tda_pubkey={}, error={e:?}]",
                     dist.receiver, dist.amount_lamports, dist.tda_pubkey,
                 );
@@ -71,17 +74,17 @@ fn main() {
     .expect("Error setting Ctrl-C handler");
 
     let maybe_rpc_client_and_keypair = if let Some(keypair_path) = &args.funding_wallet_path {
-        println!("Keypair path supplied, reading in key.");
+        info!("Keypair path supplied, reading in key.");
         let keypair = Keypair::read_from_file(keypair_path).unwrap();
         let rpc_client = Arc::new(RpcClient::new(args.rpc_url));
         let rpc_version = runtime
             .block_on(rpc_client.get_version())
             .unwrap()
             .solana_core;
-        println!("rpc server version: {}", rpc_version);
+        info!("rpc server version: {}", rpc_version);
         Some((rpc_client, keypair))
     } else {
-        println!("No keypair supplied, running in dry run mode.");
+        info!("No keypair supplied, running in dry run mode.");
         None
     };
 
@@ -108,8 +111,8 @@ fn main() {
         .iter()
         .map(|d| d.amount_lamports)
         .sum();
-    println!("remaining lamports: {}", remaining_lamports);
-    println!(
+    info!("remaining lamports: {}", remaining_lamports);
+    info!(
         "remaining distributions: {}",
         incomplete_distributions.len()
     );
@@ -124,7 +127,7 @@ fn main() {
         ));
         while let Ok(dist) = completed_distributions_rx.recv() {
             if let Err(e) = append_to_csv_file(&dist, &args.completed_distributions_path) {
-                println!(
+                error!(
                     "error writing distribution to file: [receiver={}, amount_lamports={}, tda_pubkey={}, error={e:?}]",
                     dist.receiver, dist.amount_lamports, dist.tda_pubkey,
                 );
@@ -146,7 +149,7 @@ async fn send_transactions(
             last_updated: Instant::now(),
         },
         Err(e) => {
-            println!("error fetching blockhash: {e:?}");
+            error!("error fetching blockhash: {e:?}");
             return;
         }
     };
@@ -169,7 +172,7 @@ async fn send_transactions(
                 break;
             }
         }
-        println!(
+        info!(
             "completed: {}, remaining: {}",
             completed_distributions.len(),
             incomplete_distributions.len() - completed_distributions.len()
@@ -179,7 +182,7 @@ async fn send_transactions(
             .retain(|d| completed_distributions.iter().find(|c| c == &d).is_none());
         for c in completed_distributions {
             if let Err(e) = completed_distributions_tx.send(c) {
-                println!(
+                error!(
                     "error sending distribution: [receiver={}, amount_lamports={}, tda_pubkey={}, error={e:?}]",
                     c.receiver, c.amount_lamports, c.tda_pubkey,
                 );
@@ -222,10 +225,12 @@ async fn send_and_confirm_transactions(
             recent_blockhash.hash,
         ));
         futs.push(async move {
-            if let Ok(_sig) = rpc_client.send_and_confirm_transaction(&tx).await {
-                Some(*d)
-            } else {
-                None
+            match rpc_client.send_and_confirm_transaction(&tx).await {
+                Ok(_) => Some(*d),
+                Err(e) => {
+                    debug!("error confirming tx: {e:?}");
+                    None
+                }
             }
         });
     }
@@ -236,7 +241,6 @@ async fn send_and_confirm_transactions(
             completed_distributions.push(d);
         }
     }
-    println!("completed {} distributions", completed_distributions.len());
 
     completed_distributions
 }
