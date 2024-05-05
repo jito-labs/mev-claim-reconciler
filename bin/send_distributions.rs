4829782b5dd4cfd3d0f555cc0c6e652807af86b4
segfaultdoctor <17258903+segfaultdoc@users.noreply.github.com>
2024-05-05 11:44:09 -0400

    wip

--- bin/send_distributions.rs
+++ bin/send_distributions.rs
@@ -11,8 +11,12 @@ use mev_claim_reconciler::{append_to_csv_file, read_csv_from_file, FlattenedDist
 use solana_program::hash::Hash;
 use solana_program::pubkey::Pubkey;
 use solana_program::system_instruction::transfer;
+use solana_program::system_program;
 use solana_rpc_client::nonblocking::rpc_client::RpcClient;
+use solana_rpc_client::rpc_client::SerializableTransaction;
+use solana_rpc_client_api::client_error::ErrorKind;
 use solana_rpc_client_api::config::RpcSendTransactionConfig;
+use solana_rpc_client_api::request::RpcError;
 use solana_sdk::commitment_config::{CommitmentConfig, CommitmentLevel};
 use solana_sdk::compute_budget::ComputeBudgetInstruction;
 use solana_sdk::signature::{EncodableKey, Keypair, Signer};
@@ -25,6 +29,8 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::time::{Duration, Instant};
 use tokio::runtime::Runtime;
+use tokio::sync::Semaphore;
+use tokio::time::sleep;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -35,15 +41,23 @@ struct Args {
 
     /// RPC to send transactions through.
     #[arg(long, env)]
+    jito_url: String,
+
+    /// RPC to read data from.
+    #[arg(long, env)]
     rpc_url: String,
 
     /// Path to file keeping track of completed distributions.
     #[arg(long, env)]
     completed_distributions_path: PathBuf,
 
-    /// Path to the wallet funding the distributions. If this is not supplied, then this is effectively running in dry-mode.
+    /// Path to file keeping track of distributions skipped due to closed accounts.
     #[arg(long, env)]
-    funding_wallet_path: Option<PathBuf>,
+    skipped_distributions_path: PathBuf,
+
+    /// Path to the wallet funding the distributions
+    #[arg(long, env)]
+    funding_wallet_path: PathBuf,
 }
 
 // Figment wants funds sent to a different account, so skip over them.
@@ -83,23 +97,20 @@ fn main() {
     })
     .expect("Error setting Ctrl-C handler");
 
-    let maybe_rpc_client_and_keypair = if let Some(keypair_path) = &args.funding_wallet_path {
-        info!("Keypair path supplied, reading in key.");
-        let keypair = Keypair::read_from_file(keypair_path).unwrap();
-        let rpc_client = Arc::new(RpcClient::new_with_commitment(
-            args.rpc_url,
-            CommitmentConfig::confirmed(),
-        ));
-        let rpc_version = runtime
-            .block_on(rpc_client.get_version())
-            .unwrap()
-            .solana_core;
-        info!("rpc server version: {}", rpc_version);
-        Some((rpc_client, keypair))
-    } else {
-        info!("No keypair supplied, running in dry run mode.");
-        None
-    };
+    let funder = Keypair::read_from_file(args.funding_wallet_path).unwrap();
+    let rpc_client = Arc::new(RpcClient::new_with_commitment(
+        args.rpc_url,
+        CommitmentConfig::confirmed(),
+    ));
+    let jito_client = Arc::new(RpcClient::new_with_commitment(
+        args.jito_url,
+        CommitmentConfig::confirmed(),
+    ));
+    let rpc_version = runtime
+        .block_on(rpc_client.get_version())
+        .unwrap()
+        .solana_core;
+    info!("rpc server version: {}", rpc_version);
 
     println!("reading in distributions");
     let distributions: Vec<FlattenedDistribution> =
@@ -121,6 +132,73 @@ fn main() {
         }
     }
 
+    // Closed accounts don't get claimed for.
+    info!("checking for closed accounts");
+    let mut futs = Vec::with_capacity(incomplete_distributions.len());
+    let semaphore = Arc::new(Semaphore::new(50));
+    for d in incomplete_distributions {
+        let c = rpc_client.clone();
+        let s = semaphore.clone();
+        let exit = exit.clone();
+        futs.push(async move {
+            const MAX_RETRIES: usize = 5;
+            let mut retries = 0;
+            loop {
+                if exit.load(Ordering::Relaxed) {
+                    return Ok((d, false));
+                }
+                let _permit = s.acquire().await.unwrap();
+                let res = c.get_account(&d.receiver).await;
+                drop(_permit);
+                match res {
+                    Ok(account) => {
+                        return if account.data.len() == 0 && account.owner == system_program::id() {
+                            Ok((d, false))
+                        } else {
+                            Ok((d, true))
+                        };
+                    }
+                    Err(e) => {
+                        match &e.kind {
+                            ErrorKind::RpcError(e) => match e {
+                                RpcError::ForUser(msg) => {
+                                    if msg.contains("AccountNotFound") {
+                                        return Ok((d, false));
+                                    }
+                                }
+                                _ => {}
+                            },
+                            _ => {}
+                        };
+                        error!("error fetching account: {e:?}");
+                        if retries >= MAX_RETRIES {
+                            return Err(e);
+                        }
+                        retries += 1;
+                        sleep(Duration::from_millis(100)).await;
+                    }
+                }
+            }
+        });
+    }
+
+    let mut incomplete_distributions = Vec::with_capacity(futs.len());
+    let results = rt.block_on(join_all(futs));
+    for maybe_result in results {
+        let (d, should_distribute) = maybe_result.unwrap();
+        if should_distribute {
+            incomplete_distributions.push(d);
+        } else {
+            info!("account {} closed, skipping", d.receiver);
+            append_to_csv_file(&d, &args.skipped_distributions_path).unwrap();
+        }
+    }
+
+    if incomplete_distributions.is_empty() {
+        info!("no distributions left");
+        return;
+    }
+
     let remaining_lamports: u64 = incomplete_distributions
         .iter()
         .map(|d| d.amount_lamports)
@@ -131,31 +209,40 @@ fn main() {
         incomplete_distributions.len()
     );
 
-    if let Some((rpc_client, funder)) = maybe_rpc_client_and_keypair {
-        let _send_task = rt.spawn(send_transactions(
-            completed_distributions_tx,
-            rpc_client,
-            funder,
-            incomplete_distributions,
-            exit,
-        ));
-        while let Ok(dist) = completed_distributions_rx.recv() {
-            if let Err(e) = append_to_csv_file(&dist, &args.completed_distributions_path) {
-                error!(
+    rt.spawn(async {
+        loop {
+            send_transactions(
+                &completed_distributions_tx,
+                &jito_client,
+                &rpc_client,
+                &funder,
+                &mut incomplete_distributions,
+                &exit,
+            )
+            .await;
+
+            warn!("Issues sending transactions, sleeping for a bit.");
+            sleep(Duration::from_millis(5_000)).await;
+            exit.store(true, Ordering::Relaxed);
+        }
+    });
+    while let Ok(dist) = completed_distributions_rx.recv() {
+        if let Err(e) = append_to_csv_file(&dist, &args.completed_distributions_path) {
+            error!(
                     "error writing distribution to file: [receiver={}, amount_lamports={}, tda_pubkey={}, error={e:?}]",
                     dist.receiver, dist.amount_lamports, dist.tda_pubkey,
                 );
-            }
         }
     }
 }
 
 async fn send_transactions(
-    completed_distributions_tx: Sender<FlattenedDistribution>,
-    rpc_client: Arc<RpcClient>,
-    funder: Keypair,
-    mut incomplete_distributions: Vec<FlattenedDistribution>,
-    exit: Arc<AtomicBool>,
+    completed_distributions_tx: &Sender<FlattenedDistribution>,
+    jito_client: &Arc<RpcClient>,
+    rpc_client: &Arc<RpcClient>,
+    funder: &Keypair,
+    incomplete_distributions: &mut Vec<FlattenedDistribution>,
+    exit: &Arc<AtomicBool>,
 ) {
     let mut recent_blockhash = match rpc_client
         .get_latest_blockhash_with_commitment(CommitmentConfig::confirmed())
@@ -178,6 +265,7 @@ async fn send_transactions(
             completed_distributions.extend(
                 send_and_confirm_transactions(
                     batch,
+                    &jito_client,
                     &rpc_client,
                     &funder,
                     &mut recent_blockhash,
@@ -208,6 +296,7 @@ async fn send_transactions(
     }
 }
 
+#[derive(Copy, Clone)]
 struct RecentBlockhash {
     hash: Hash,
     last_updated: Instant,
@@ -215,6 +304,7 @@ struct RecentBlockhash {
 
 async fn send_and_confirm_transactions(
     batch: &[FlattenedDistribution],
+    jito_client: &Arc<RpcClient>,
     rpc_client: &Arc<RpcClient>,
     funder: &Keypair,
     recent_blockhash: &mut RecentBlockhash,
@@ -248,22 +338,43 @@ async fn send_and_confirm_transactions(
             &[funder],
             recent_blockhash.hash,
         ));
+        let c_exit = exit.clone();
+        let blockhash = recent_blockhash.hash;
         futs.push(async move {
-            match rpc_client
-                .send_and_confirm_transaction_with_spinner_and_config(
-                    &tx,
-                    CommitmentConfig::confirmed(),
-                    RpcSendTransactionConfig {
-                        skip_preflight: true,
-                        preflight_commitment: None,
-                        encoding: None,
-                        max_retries: Some(3),
-                        min_context_slot: None,
-                    },
-                )
-                .await
-            {
-                Ok(_) => Some(*d),
+            match jito_client.send_transaction(&tx).await {
+                Ok(_) => loop {
+                    match rpc_client.confirm_transaction(tx.get_signature()).await {
+                        Ok(is_confirmed) => {
+                            if is_confirmed {
+                                return Some(*d);
+                            } else {
+                                match rpc_client
+                                    .is_blockhash_valid(&blockhash, CommitmentConfig::confirmed())
+                                    .await
+                                {
+                                    Ok(is_valid) => {
+                                        if is_valid {
+                                            sleep(Duration::from_millis(100)).await;
+                                            continue;
+                                        } else {
+                                            return None;
+                                        }
+                                    }
+                                    Err(e) => {
+                                        error!("error checking blockhash: {e:?}");
+                                        c_exit.store(true, Ordering::Relaxed);
+                                        return None;
+                                    }
+                                }
+                            }
+                        }
+                        Err(e) => {
+                            error!("error confirming tx: {e:?}");
+                            c_exit.store(true, Ordering::Relaxed);
+                            return None;
+                        }
+                    }
+                },
                 Err(e) => {
                     debug!(
                         "error confirming tx: {e:?}, b64 data: {}",
